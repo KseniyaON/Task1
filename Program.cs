6df989a71473c20c3cdff304679374dd3436a813
Kseniya Ovinnikova <studos@list.ru>
2024-05-31 17:43:48 +0500

    new dirrection for file program

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿           int Nature_Numbers( int M, int N )
+           {
+           
+           if(M<=0)
+            { 
+                Nature_Numbers (M+1,N);
+                return M;
+             }
+              else
+                {
+                  if(M==N)  
+                   {
+                    Console.WriteLine(M + " ");
+                    return M;
+                   }
+             
+                }
+            
+              Console.Write(M + " ");
+              Nature_Numbers (M+1,N);
+              
+              return M ;
+            }
+           
+       Nature_Numbers (10,30);
+
