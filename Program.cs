da51f03eaaa2b26130985cd2f431440fe5adac24
Kseniya Ovinnikova <studos@list.ru>
2024-05-31 19:19:18 +0500

    was added void Main

--- Program.cs
+++ Program.cs
@@ -1,2 +1,23 @@
 ﻿// Задача 3: Задайте произвольный массив. Выведете его элементы, начиная с конца. Использовать рекурсию, не использовать циклы.
 
+    public static void Main(string[] args)
+    {
+        int [] M=new int [5]{1,2,3,4,5};
+        int il=M.Length;
+        PrintMas(M, il);
+    }
+
+void PrintMas(int[] arr, int il)
+{
+    il = il - 1;
+    if (il > 0 || il == 0)
+    {
+        Console.Write(arr[il] + " ");
+        PrintMas(arr, il);
+    }
+    
+    
+    return;
+}
+
+
