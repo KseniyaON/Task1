6d00ccb5c9e0aeec67c930f81987cd26407e1520
Kseniya Ovinnikova <studos@list.ru>
2024-05-31 18:42:09 +0500

    was created program for calculation of Akkerman function

--- Program.cs
+++ Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 2: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+
+int M = 2;
+int N = 3;
+int acr;
+
+acr = AccerMan(M, N);
+
+int AccerMan(int M, int N)
+{
+    if (M == 0)
+    {
+        return N + 1;
+
+    }
+    else
+    {
+        if (M > 0 && N == 0)
+        {
+            return AccerMan(M - 1, 1);
+        }
+        else
+        {
+            if (M > 0 && N > 0)
+            {
+                return AccerMan(M - 1, AccerMan(M, N - 1));
+            }
+        }
+    }
+    return N + 1;
+
+}
+
+// void CalcAccer()
+// {
+
+
+
+Console.Write(acr);
+//}
+
