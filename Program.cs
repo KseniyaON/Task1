2d83c4e98265265116d93857f1dc61d1e427af4a
Kseniya Ovinnikova <studos@list.ru>
2024-05-31 19:28:47 +0500

    Was used old school method for resolving Task 3 :)

--- Program.cs
+++ Program.cs
@@ -1,23 +1,28 @@
 ﻿// Задача 3: Задайте произвольный массив. Выведете его элементы, начиная с конца. Использовать рекурсию, не использовать циклы.
 
-    public static void Main(string[] args)
+internal class Program
+{
+    private static void Main(string[] args)
     {
-        int [] M=new int [5]{1,2,3,4,5};
-        int il=M.Length;
+
+        int[] M = new int[5] { 1, 2, 3, 4, 5 };
+        int il = M.Length;
+
+
+
+        void PrintMas(int[] arr, int il)
+        {
+            il = il - 1;
+            if (il > 0 || il == 0)
+            {
+                Console.Write(arr[il] + " ");
+                PrintMas(arr, il);
+            }
+
+
+            return;
+
+        }
         PrintMas(M, il);
     }
-
-void PrintMas(int[] arr, int il)
-{
-    il = il - 1;
-    if (il > 0 || il == 0)
-    {
-        Console.Write(arr[il] + " ");
-        PrintMas(arr, il);
-    }
-    
-    
-    return;
 }
-
-
